bf9513c6b09a06ab3835e2770b6ec80b4378757f
François-Alexian Lopes <60343862+lopesf-a@users.noreply.github.com>
2025-10-28 15:25:32 +0100

    Fix import path for CartItemController

--- src/routes/CartsItemRoutes.tsx
+++ src/routes/CartsItemRoutes.tsx
@@ -1,4 +1,4 @@
-import { CartItemController } from "../controllers/CartsItemControllers.ts";
+import { CartItemController } from "../controllers/CartsItemControllers.tsx";
 
 export async function cartItemRoutes(req: Request, path: string) {
     if (req.method === "POST" && path === "/api/cartitem") return CartItemController.create(req);
