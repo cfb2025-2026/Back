1d16a90db08916a526e3d32f01d034f2e117223d
François-Alexian Lopes <francois.lopes@yahoo.com>
2025-11-17 16:28:33 +0100

    product id = id

--- src/models/Product.tsx
+++ src/models/Product.tsx
@@ -11,7 +11,7 @@ export const ProductModel = {
         const { data, error } = await supabase
             .from("Product")
             .select("*")
-            .eq("id", id) // id = string UUID
+            .eq("product_id", id) // <-- mettre le vrai nom de colonne ici
             .single();
 
         if (error) throw new Error(error.message);
@@ -25,13 +25,13 @@ export const ProductModel = {
     },
 
     async update(id: string, product: Partial<{ name: string; price: number; img_url: string; seller_id: number; review_id: number }>) {
-        const { data, error } = await supabase.from("Product").update(product).eq("id", id).select();
+        const { data, error } = await supabase.from("Product").update(product).eq("product_id", id).select();
         if (error) throw new Error(error.message);
         return data[0];
     },
 
     async delete(id: string) {
-        const { error } = await supabase.from("Product").delete().eq("id", id);
+        const { error } = await supabase.from("Product").delete().eq("product_id", id);
         if (error) throw new Error(error.message);
         return { message: "Product deleted" };
     },
